bfcf50e5a36a8b409ff0180fa5c8b162cc79dc75
urolyi1 <urolyi1@gmail.com>
2020-02-04 10:38:28 -0500

    Add realistic blood type generator with and without tissue

--- HospitalGenerators.py
+++ HospitalGenerators.py
@@ -1,5 +1,104 @@
 import numpy as np
 
+
+class RealisticHospitalNoTissue:
+    def __init__(self, k):
+        # [O, A, B, AB] Probabilities
+        self.bloodtypes = ['', 'A', 'B', 'AB']
+        self.bloodtype_probs = [0.48, 0.34, 0.14, 0.04]
+        self.bloodtype_cutoffs = [0.48, 0.82, 0.96, 1.0]
+
+        self.patients = k
+
+    def generate(self, batch_size):
+        num_types = len(self.bloodtype_probs)
+
+        # patient type, donor_type, patient tissue_type, donor tissue type
+        bids = np.zeros((batch_size, num_types, num_types))
+
+        for i in range(batch_size):
+            for j in range(self.patients):
+                bids[(i,) + self.generate_pair()] += 1.0
+
+        return bids.reshape((batch_size, -1))
+
+    def generate_pair(self):
+        incompat = False
+        ret_tuple = None
+        while (not incompat):
+            blood_idx = np.random.choice(len(self.bloodtype_probs), p=self.bloodtype_probs, size=2)
+            p_blood_idx = blood_idx[0]
+            d_blood_idx = blood_idx[1]
+
+            if (not self.is_blood_compat(p_blood_idx, d_blood_idx)):
+                incompat = True
+                ret_tuple = (p_blood_idx, d_blood_idx)
+
+        return ret_tuple
+
+    def is_blood_compat(self, p_idx, d_idx):
+        p_type = self.bloodtypes[p_idx]
+        d_type = self.bloodtypes[d_idx]
+        for ch in d_type:
+            if ch not in p_type:
+                return False
+        return True
+
+
+class RealisticHospital:
+    def __init__(self, k):
+        # [O, A, B, AB] Probabilities
+        self.bloodtypes = ['', 'A', 'B', 'AB']
+        self.bloodtype_probs = [0.48, 0.34, 0.14, 0.04]
+        self.bloodtype_cutoffs = [0.48, 0.82, 0.96, 1.0]
+
+        # Tissue incompatibilitiy probabilities
+        self.tissue_probs = [.7, .2, .1]
+        self.tissue_cutoffs = [.7, .9, 1.0]
+
+        # Tissue incompat values
+        self.tissue_vals = [0.05, 0.45, 0.9]
+        self.patients = k
+
+    def generate(self, batch_size):
+        num_types = len(self.bloodtype_probs)
+        num_tissue = len(self.tissue_probs)
+
+        # patient type, donor_type, patient tissue_type, donor tissue type
+        bids = np.zeros((batch_size, num_types, num_types, num_tissue, num_tissue))
+
+        for i in range(batch_size):
+            for j in range(self.patients):
+                bids[(i,) + self.generate_pair()] += 1.0
+
+        return bids.reshape((batch_size, -1))
+
+    def generate_pair(self):
+        incompat = False
+        ret_tuple = None
+        while (not incompat):
+            tissue_idx = np.random.choice(len(self.tissue_probs), p=self.tissue_probs, size=2)
+            p_tissue_idx = tissue_idx[0]
+            d_tissue_idx = tissue_idx[1]
+
+            blood_idx = np.random.choice(len(self.bloodtype_probs), p=self.bloodtype_probs, size=2)
+            p_blood_idx = blood_idx[0]
+            d_blood_idx = blood_idx[1]
+
+            if (self.tissue_vals[d_tissue_idx] < self.tissue_vals[p_tissue_idx]
+                    or not self.is_blood_compat(p_blood_idx, d_blood_idx)):
+                incompat = True
+                ret_tuple = (p_blood_idx, d_blood_idx, p_tissue_idx, d_tissue_idx)
+        return ret_tuple
+
+    def is_blood_compat(self, p_idx, d_idx):
+        p_type = self.bloodtypes[p_idx]
+        d_type = self.bloodtypes[d_idx]
+        for ch in d_type:
+            if ch not in p_type:
+                return False
+        return True
+
 class SingleHospital:
     def __init__(self, n_types, dist_lst):
         ''' Takes in number of pair types along with a list of functions that
