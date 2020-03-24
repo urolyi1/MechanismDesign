7e128ff17d48c579bfb30c9e417845887f1b55a4
urolyi1 <urolyi1@gmail.com>
2020-03-24 16:19:50 -0400

    Train ashlagi negative on random sample

--- ashlagi_negative_experiment.py
+++ ashlagi_negative_experiment.py
@@ -13,6 +13,7 @@ from matchers import MatchNet, GreedyMatcher
 from util import convert_internal_S
 
 SAVE = False
+np.random.seed(0)
 
 # Command line argument parser
 parser = argparse.ArgumentParser()
@@ -21,7 +22,7 @@ parser.add_argument('--main-lr', type=float, default=5e-2, help='main learning r
 parser.add_argument('--main-iter', type=int, default=40, help='number of outer iterations')
 parser.add_argument('--init-iter', type=int, default=100, help='number of outer iterations')
 parser.add_argument('--batchsize', type=int, default=2, help='batch size')
-parser.add_argument('--nbatch', type=int, default=1, help='number of batches')
+parser.add_argument('--nbatch', type=int, default=2, help='number of batches')
 parser.add_argument('--misreport-iter', type=int, default=100, help='number of misreport iterations')
 parser.add_argument('--misreport-lr', type=float, default=.25, help='misreport learning rate')
 parser.add_argument('--random-seed', type=int, default=0, help='random seed')
@@ -30,13 +31,26 @@ args = parser.parse_args()
 
 N_HOS = 2
 N_TYP = 4
+
 internal_s = torch.tensor([
                            [1., 0.],
                            [1., 1.],
                            [0., 1.],
                            [0., 1.]
                            ])
-batches = torch.tensor([
+central_s = torch.tensor(convert_internal_S(internal_s.numpy(), N_HOS),
+                         requires_grad=False, dtype=torch.float32)
+# probability for generator
+prob_lst1 = [.5, .5, 0.0, 0.0]
+prob_lst2 = [0.0, 0.0, .5, .5]
+
+hospital_generator_list = [gens.GenericTypeHospital(prob_lst1, 30), gens.GenericTypeHospital(prob_lst2, 30)]
+report_gen = gens.ReportGenerator(hospital_generator_list, (N_HOS, N_TYP))
+batches = mn.create_train_sample(report_gen, args.nbatch, batch_size=args.batchsize)
+test_batches = mn.create_train_sample(report_gen, 2, batch_size=10)
+
+'''
+test_batches = torch`.tensor([
     [
         [[10.0,10.0,0.0,0.0],
          [0.0,0.0,10.0,10.0]],
@@ -44,8 +58,10 @@ batches = torch.tensor([
          [0.0,0.0,3.0,3.0]]
     ]
 ])
-central_s = torch.tensor(convert_internal_S(internal_s.numpy(), N_HOS),
-                         requires_grad=False, dtype=torch.float32)
+'''
+
+
+
 num_structures = central_s.shape[1]
 batch_size = batches.shape[1]
 int_structures = internal_s.shape[1]
@@ -64,4 +80,4 @@ model = MatchNet(N_HOS, N_TYP, num_structures, int_structures, central_s, intern
                  control_strength=args.control_strength)
 # Create experiment
 ashlagi_experiment = Experiment.Experiment(args, internal_s, N_HOS, N_TYP, model, dir=prefix)
-ashlagi_experiment.run_experiment(batches, batches, save=SAVE, verbose=True)
+ashlagi_experiment.run_experiment(batches, test_batches, save=SAVE, verbose=True)
