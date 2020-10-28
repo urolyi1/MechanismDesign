80db8bb9e4a16cd8d4ded49e13483ee6efcd1546
Michael J. Curry <curry@cs.umd.edu>
2020-10-28 13:02:36 -0400

    simple plotting code

---
+++ double_net/plot_utils.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+
+def plot_12_model(model, grid_width=0.1, item1_range=(0, 1), item2_range=(0, 1), name="test"):
+    item1_min, item1_max = item1_range
+    item2_min, item2_max = item2_range
+    xcoords = (item1_max - item1_min) * torch.arange(0, 1, grid_width).view(1, -1) + item1_min
+    ycoords = (item2_max - item2_min) * torch.arange(0, 1, grid_width).view(-1, 1) + item2_min
+    xlen = ycoords.shape[0]
+    ylen = xcoords.shape[1]
+
+    xcoords_tiled = xcoords.repeat(xlen, 1)
+    ycoords_tiled = ycoords.repeat(1, ylen)
+
+    combined = torch.stack((xcoords_tiled, ycoords_tiled), dim=2)
+
+    output_allocs, output_payments = model(combined.view(-1, 1, 2))
+
+    output_item1_allocs = output_allocs[:, :, 0].view(xlen, ylen)
+    output_item2_allocs = output_allocs[:, :, 1].view(xlen, ylen)
+    f, (ax1, ax2) = plt.subplots(2, 1)
+    im1 = ax1.imshow(output_item1_allocs.cpu().detach().numpy(), origin='lower', cmap='YlOrRd',extent=[0,1,0,1])
+    ax1.set_title('prob of allocating item 1')
+    ax1.set_xlabel('v1')
+    ax1.set_ylabel('v2')
+    im2 = ax2.imshow(output_item2_allocs.cpu().detach().numpy(), origin='lower', cmap='YlOrRd',extent=[0,1,0,1])
+    ax2.set_title('prob of allocating item 2')
+    ax2.set_xlabel('v1')
+    ax2.set_ylabel('v2')
+    f.tight_layout()
+    f.show()
+
+#     plt.savefig(f'result/{name}_model12.png')
+#     plt.close()
+
