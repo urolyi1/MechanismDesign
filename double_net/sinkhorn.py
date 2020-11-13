3115540b30d0e483f6ea9ce2a29c60ef1fbe6d33
urolyi1 <urolyi1@gmail.com>
2020-11-13 17:48:07 -0500

    Add log_sinkhorn_plan iterative method

--- double_net/sinkhorn.py
+++ double_net/sinkhorn.py
@@ -17,6 +17,19 @@ def sinkhorn_plan(dist_mat, a, b, epsilon=1e-1, rounds=2):
     return torch.einsum('...i,...ik,...k->...ik', u, K, v)
 
 
+def log_sinkhorn_plan(dist_mat, a, b, epsilon, rounds):
+    batch_size = dist_mat.shape[0]
+    v = torch.ones_like(b)
+    g = epsilon * torch.log(v)
+    for i in range(rounds):
+        f = -epsilon * torch.logsumexp(-(dist_mat - g.view(batch_size, 1, -1)) / epsilon, dim=-1) + \
+            epsilon * torch.log(a)
+        g = -epsilon * torch.logsumexp(-(dist_mat - f.view(batch_size, -1, 1)) / epsilon, dim=1) + \
+            epsilon * torch.log(b)
+
+    return torch.exp((-dist_mat + f.view(batch_size, -1, 1) + g.view(batch_size, 1, -1)) / epsilon)
+
+
 def generate_marginals(n_agents, n_items):
     main_agents = [1.0 for _ in range(n_agents)]
     main_items = [1.0 for _ in range(n_items)]
